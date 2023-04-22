176350aa52cc4de87c7fe470ed8381d513ad35d6
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-22 23:30:00 +0300

    Add project files.

---
+++ tema3ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//Scrieti o functie care va determina daca un numar este sau nu numar prim. Apelati-o si afisati-i rezultatul
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        public static bool EsteNumarPrim(int numar)
+        {
+            if (numar < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= numar; i++)
+            {
+                if (numar % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int numar = int.Parse(Console.ReadLine());
+            bool estePrim = EsteNumarPrim(numar);
+
+            if (estePrim)
+            {
+                Console.WriteLine("prim");
+            }
+            else
+            {
+                Console.WriteLine(" nu este prim.");
+            }
+        }
+    }
+}
